519d2da8b38789cc8d0428331a4799b1612bf017
AntoineCharette <antoineocharette@gmail.com>
2021-11-15 15:26:59 -0500

    adds basic server and client

--- index.js
+++ index.js
@@ -8,15 +8,20 @@ const ezchat = server.of('/ezchat');
 
 ezchat.on('connection', (socket) => {
   console.log('Connected to socket', socket.id);
+  // socket.join(roomKey);
+
+  socket.on('join', (payload) => {
+    for (const room of payload.rooms) {
+      socket.join(room);
+    }
+  });
 
   socket.on('send', (payload) => {
-    orderId = payload.orderId;
-    socket.join(orderId);
-    ezchat.emit('join', payload);
+    ezchat.to(payload.room).emit('message', payload);
   });
 
-  socket.on('in-transit', (payload) => {
-    socket.join(orderId);
-    ezchat.to(orderId).emit('delivered', payload);
-  });
+  // socket.on('in-transit', (payload) => {
+  //   socket.join(orderId);
+  //   ezchat.to(orderId).emit('delivered', payload);
+  // });
 });
